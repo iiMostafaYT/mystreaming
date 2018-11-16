f61eb465f328b474e0a243a4571047655dbae4d3
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-11-16 19:45:21 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+const adminprefix = "-";
+const devs = ['383606619671494659','383606619671494659'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+      } else     
+if (message.content.startsWith(adminprefix + 'sett')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");//حقوق دايموند كودزحقوق دايموند كودز
+}
+});
+
+client.login(process.env.BOT_TOKEN);
